eb971757e58e0f07d3c91ee3a71cad2519713302
lxt <leixiaotian434@gmail.com>
2024-09-01 15:46:11 +0800

    modified:   abstract-machine/am/src/platform/nemu/ioe/input.c

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,7 +2,7 @@
  * @Author: lxt leixiaotian434@gmail.com
  * @Date: 2024-01-15 09:47:31
  * @LastEditors: lxt leixiaotian434@gmail.com
- * @LastEditTime: 2024-09-01 11:11:14
+ * @LastEditTime: 2024-09-01 15:24:21
  * @FilePath: /ysyx-workbench/abstract-machine/am/src/platform/nemu/ioe/input.c
  * @Description: 
  * 
@@ -15,7 +15,13 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-	kbd->keycode = (uint32_t)inl(KBD_ADDR);
-	if (AM_KEY_NONE) kbd->keydown = 0;
-	else kbd->keydown = 1;
+	int code = (uint32_t)inl(KBD_ADDR);
+	if(KEYDOWN_MASK < code) {
+		kbd->keycode = code - KEYDOWN_MASK;
+		kbd->keydown = 1;
+	}
+	else {
+		kbd->keycode = code;
+		kbd->keydown = 0;
+	}
 }
