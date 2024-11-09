ed9d38a051ea33d8c8b2ea251dd4b37c69ed7fa5
23060306-Lei Xiao Tian <leixiaotian434@gmail.com>
2024-11-09 22:50:35 +0800

    成功修改AXI-Lite为AXI

---
+++ npc/csrc/device/uart.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: 23060306-Lei Xiao Tian leixiaotian434@gmail.com
+ * @Date: 2024-11-09 22:25:41
+ * @LastEditors: 23060306-Lei Xiao Tian leixiaotian434@gmail.com
+ * @LastEditTime: 2024-11-09 22:28:16
+ * @FilePath: /ysyx-workbench/npc/csrc/device/uart.c
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ */
+#include <utils.h>
+
+extern "C" void uart(unsigned char chr);
+void uart(unsigned char chr) {
+    putchar(chr);
+}
