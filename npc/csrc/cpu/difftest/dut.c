a2e8146deba735ec299894c1a3fd3de72c8c2b67
tracer-ysyx <tracer@ysyx.org>
2025-01-17 21:07:10 +0800

    > compile NEMU ysyx_23060306 雷啸天 Linux lxt-B650M-GAMING-PLUS-WIFI 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:10 up 42 min,  1 user,  load average: 0.12, 0.14, 0.16

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -119,9 +119,9 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 	if (skip_dut_nr_inst > 0) {
 		ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 		if (ref_r.pc == npc) {
-		skip_dut_nr_inst = 0;
-		checkregs(&ref_r, npc);
-		return;
+			skip_dut_nr_inst = 0;
+			checkregs(&ref_r, npc);
+			return;
 		}
 		skip_dut_nr_inst --;
 		if (skip_dut_nr_inst == 0)
