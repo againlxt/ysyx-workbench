e44c1e34d7ab3f682f95721ed9447da861d90643
LEI XIAO TIAN <leixiaotian434@gmail.com>
2025-07-12 15:15:01 +0800

    modified:   nemu/src/monitor/monitor.c

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -83,7 +83,7 @@ static int parse_args(int argc, char *argv[]) {
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
-	{"elf"		, required_argument, NULL, 'e'},
+	  {"elf"		, required_argument, NULL, 'e'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
@@ -94,7 +94,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-	  case 'e':	elf_file = optarg; break;
+	    case 'e':	elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -102,7 +102,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-		printf("\t-e,--elf=ELF			  input elf file\n");
+		    printf("\t-e,--elf=ELF			  input elf file\n");
         printf("\n");
         exit(0);
     }
@@ -157,6 +157,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Display welcome message. */
   welcome();
 }
+
 #else // CONFIG_TARGET_AM
 static long load_img() {
   extern char bin_start, bin_end;
