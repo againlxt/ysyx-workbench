04ec727fbf22e9f4e6e4333c900399f6ca388375
LEI XIAO TIAN <leixiaotian434@gmail.com>
2025-08-25 14:25:52 +0800

    modified:   npc/chisel/cpu 	modified:   npc/vsrc/soc_cpu/BranchPredict.v 	modified:   npc/vsrc/soc_cpu/IFU.v 	deleted:    npc/vsrc/soc_cpu/Queue.v 	modified:   npc/vsrc/soc_cpu/directionPredictor.v 	modified:   npc/vsrc/yosys/BranchPredict.v 	modified:   npc/vsrc/yosys/IFU.v 	deleted:    npc/vsrc/yosys/Queue.v 	modified:   npc/vsrc/yosys/directionPredictor.v
     Changes not staged for commit:
    	modified:   npc/chisel/cpu (modified content)
    	modified:   yosys-opensta (modified content)
    	modified:   yosys-sta (modified content)
    	modified:   ysyxSoC (modified content)

--- npc/vsrc/yosys/IFU.v
+++ npc/vsrc/yosys/IFU.v
@@ -24,17 +24,6 @@ module IFU(
   wire  branchPredict_io_ifu2ICache_ready; // @[src/main/scala/ifu/IFU.scala 31:41]
   wire  branchPredict_io_ifu2ICache_valid; // @[src/main/scala/ifu/IFU.scala 31:41]
   wire [31:0] branchPredict_io_ifu2ICache_bits_pc; // @[src/main/scala/ifu/IFU.scala 31:41]
-  wire  pcFIFO_clock; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire  pcFIFO_reset; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire  pcFIFO_io_enq_ready; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire  pcFIFO_io_enq_valid; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire [31:0] pcFIFO_io_enq_bits_0; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire [31:0] pcFIFO_io_enq_bits_1; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire  pcFIFO_io_deq_ready; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire  pcFIFO_io_deq_valid; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire [31:0] pcFIFO_io_deq_bits_0; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire [31:0] pcFIFO_io_deq_bits_1; // @[src/main/scala/ifu/IFU.scala 32:49]
-  wire  pcFIFO_io_flush; // @[src/main/scala/ifu/IFU.scala 32:49]
   BranchPredict branchPredict ( // @[src/main/scala/ifu/IFU.scala 31:41]
     .clock(branchPredict_clock),
     .reset(branchPredict_reset),
@@ -45,36 +34,16 @@ module IFU(
     .io_ifu2ICache_valid(branchPredict_io_ifu2ICache_valid),
     .io_ifu2ICache_bits_pc(branchPredict_io_ifu2ICache_bits_pc)
   );
-  Queue pcFIFO ( // @[src/main/scala/ifu/IFU.scala 32:49]
-    .clock(pcFIFO_clock),
-    .reset(pcFIFO_reset),
-    .io_enq_ready(pcFIFO_io_enq_ready),
-    .io_enq_valid(pcFIFO_io_enq_valid),
-    .io_enq_bits_0(pcFIFO_io_enq_bits_0),
-    .io_enq_bits_1(pcFIFO_io_enq_bits_1),
-    .io_deq_ready(pcFIFO_io_deq_ready),
-    .io_deq_valid(pcFIFO_io_deq_valid),
-    .io_deq_bits_0(pcFIFO_io_deq_bits_0),
-    .io_deq_bits_1(pcFIFO_io_deq_bits_1),
-    .io_flush(pcFIFO_io_flush)
-  );
-  assign io_inst_valid = pcFIFO_io_deq_valid; // @[src/main/scala/ifu/IFU.scala 59:41]
-  assign io_inst_bits_inst = pcFIFO_io_deq_bits_1; // @[src/main/scala/ifu/IFU.scala 58:41]
-  assign io_inst_bits_pc = pcFIFO_io_deq_bits_0; // @[src/main/scala/ifu/IFU.scala 57:41]
-  assign io_ifu2ICache_valid = branchPredict_io_ifu2ICache_valid; // @[src/main/scala/ifu/IFU.scala 40:41]
-  assign io_ifu2ICache_bits_pc = branchPredict_io_ifu2ICache_bits_pc; // @[src/main/scala/ifu/IFU.scala 40:41]
-  assign io_icache2IFU_ready = pcFIFO_io_enq_ready; // @[src/main/scala/ifu/IFU.scala 54:41]
+  assign io_inst_valid = io_icache2IFU_valid; // @[src/main/scala/ifu/IFU.scala 53:41]
+  assign io_inst_bits_inst = io_icache2IFU_bits_inst; // @[src/main/scala/ifu/IFU.scala 52:41]
+  assign io_inst_bits_pc = io_icache2IFU_bits_pc; // @[src/main/scala/ifu/IFU.scala 51:41]
+  assign io_ifu2ICache_valid = branchPredict_io_ifu2ICache_valid; // @[src/main/scala/ifu/IFU.scala 39:41]
+  assign io_ifu2ICache_bits_pc = branchPredict_io_ifu2ICache_bits_pc; // @[src/main/scala/ifu/IFU.scala 39:41]
+  assign io_icache2IFU_ready = io_inst_ready; // @[src/main/scala/ifu/IFU.scala 50:41]
   assign branchPredict_clock = clock;
   assign branchPredict_reset = reset;
-  assign branchPredict_io_correctPC = io_correctPC; // @[src/main/scala/ifu/IFU.scala 37:41]
-  assign branchPredict_io_fromPC = io_fromPC; // @[src/main/scala/ifu/IFU.scala 38:41]
-  assign branchPredict_io_flush = io_flush; // @[src/main/scala/ifu/IFU.scala 39:41]
-  assign branchPredict_io_ifu2ICache_ready = io_ifu2ICache_ready; // @[src/main/scala/ifu/IFU.scala 40:41]
-  assign pcFIFO_clock = clock;
-  assign pcFIFO_reset = reset;
-  assign pcFIFO_io_enq_valid = io_icache2IFU_valid; // @[src/main/scala/ifu/IFU.scala 51:41]
-  assign pcFIFO_io_enq_bits_0 = io_icache2IFU_bits_pc; // @[src/main/scala/ifu/IFU.scala 52:33]
-  assign pcFIFO_io_enq_bits_1 = io_icache2IFU_bits_inst; // @[src/main/scala/ifu/IFU.scala 53:33]
-  assign pcFIFO_io_deq_ready = io_inst_ready; // @[src/main/scala/ifu/IFU.scala 56:41]
-  assign pcFIFO_io_flush = io_flush; // @[src/main/scala/ifu/IFU.scala 55:35]
+  assign branchPredict_io_correctPC = io_correctPC; // @[src/main/scala/ifu/IFU.scala 36:41]
+  assign branchPredict_io_fromPC = io_fromPC; // @[src/main/scala/ifu/IFU.scala 37:41]
+  assign branchPredict_io_flush = io_flush; // @[src/main/scala/ifu/IFU.scala 38:41]
+  assign branchPredict_io_ifu2ICache_ready = io_ifu2ICache_ready; // @[src/main/scala/ifu/IFU.scala 39:41]
 endmodule
